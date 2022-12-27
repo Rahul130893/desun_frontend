d03b460b2ea62ed361951daf7446c7e95da35b29
Rahul <rahulrdx1308@gmail.com>
2022-12-27 21:13:33 +0530

    signup, login, add to cart, chekcout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,9 +1,31 @@
 
+import { fetchCart } from "../Redux/games/action";
+import { useSelector, useDispatch } from "react-redux/es/exports";
+import { useEffect } from "react";
+import { CheckoutCard } from "../components/CheckoutCard";
+
 
 export const Checkout = () => {
+     const Cart = useSelector((store) => store.gameData.cart);
+     const dispatch = useDispatch();
+     console.log(Cart.length);
+
+     useEffect(() => {
+       if (Cart?.length === 0) {
+         dispatch(fetchCart());
+       }
+     }, [Cart?.length, dispatch]);
+    
+    
     return (
-        <div>
-            <h1>Checkout Page</h1>
-        </div>
-    )
+      <div className="gamepage1">
+        {Cart.map((e, i) => {
+          return (
+            <div key={i}>
+              <CheckoutCard title={e.title} image={e.image} />
+            </div>
+          );
+        })}
+      </div>
+    );
 }
